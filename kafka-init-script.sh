0bd666f453030b31fa00eb9eb86576eebfcf506b
unknown <nguyenquochuydl123@gmail.com>
2024-10-28 09:42:41 +0700

    Add script

--- kafka-init-script.sh
+++ kafka-init-script.sh
@@ -7,7 +7,7 @@ until kafka-topics --bootstrap-server server-1.pgonevn.com:9092 --list; do
 done
 
 echo "Creating Kafka topics..."
-kafka-topics --bootstrap-server server-1.pgonevn.com:9092 --create --topic my-topic-1 --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server server-1.pgonevn.com:9092 --create --topic Transfer --replication-factor 1 --partitions 1
 kafka-topics --bootstrap-server server-1.pgonevn.com:9092 --create --topic my-topic-2 --replication-factor 1 --partitions 1
 
 echo "Kafka topics created successfully!"
